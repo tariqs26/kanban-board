bd34b46e2bf4d3629f61bfdf81f60e3be2a483e7
Saad Tariq <saadtariq.dev@outlook.com>
2024-03-20 18:49:31 -0400

    feat: add global styles, add board provider render board component in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import BoardProvider from "./components/BoardProvider"
+import KanbanBoard from "./components/KanbanBoard"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>Vite Project</h1>
-  </React.StrictMode>,
+    <BoardProvider>
+      <KanbanBoard />
+    </BoardProvider>
+  </React.StrictMode>
 )
